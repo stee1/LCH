1b21f51b103b1882b20c6b081dee460c18b95f42
stee1 <krdima92@gmail.com>
2016-09-01 19:31:02 +0300

    fixed partners block, added hover style to social buttons

--- js/main.js
+++ js/main.js
@@ -22,22 +22,64 @@ $(function () {
 
     $('.top-block-el .clickable tbody tr').click(function () {
         console.log($(this));
-            $(this).siblings().removeClass('top-block-el-active');
-            $(this).addClass('top-block-el-active');
-            $(this).siblings().find('img').removeClass('filter_white');
-            $(this).find('img').addClass('filter_white');
-            $(this).siblings().find('.top-col2-info').text("");
-            $(this).find('.top-col2-info').text("не хватает?");
+        $(this).siblings().removeClass('top-block-el-active');
+        $(this).addClass('top-block-el-active');
+        $(this).siblings().find('img').removeClass('filter_white');
+        $(this).find('img').addClass('filter_white');
+        $(this).siblings().find('.top-col2-info').text("");
+        $(this).find('.top-col2-info').text("не хватает?");
     });
+
+    $('.partners_arrow_right').click(function () {
+        if ($(window).width() <= 974 && $(window).width() >= 750) {
+            if ($(this).parent().parent().find("#wtb").attr("class").indexOf("hidden-sm") === -1) {
+                $(this).parent().parent().find("#wtb").addClass("hidden-sm");
+                $(this).parent().parent().find("#ego").removeClass("hidden-sm");
+            }
+        }
+        else if ($(window).width() <= 750) {
+            if ($(this).parent().parent().find("#wtb").attr("class").indexOf("hidden-xs") === -1) {
+                console.log("here");
+                $(this).parent().parent().find("#wtb").addClass("hidden-xs");
+                $(this).parent().parent().find("#pp").removeClass("hidden-xs");
+            }
+            else if ($(this).parent().parent().find("#ab").attr("class").indexOf("hidden-xs") === -1) {
+                console.log("here");
+                $(this).parent().parent().find("#ab").addClass("hidden-xs");
+                $(this).parent().parent().find("#ego").removeClass("hidden-xs");
+            }
+        }
+    });
+
+    $('.partners_arrow_left').click(function () {
+        if ($(window).width() <= 974 && $(window).width() >= 750) {
+            if ($(this).parent().parent().find("#ego").attr("class").indexOf("hidden-sm") === -1) {
+                $(this).parent().parent().find("#ego").addClass("hidden-sm");
+                $(this).parent().parent().find("#wtb").removeClass("hidden-sm");
+            }
+        }
+        else if ($(window).width() <= 750) {
+            if ($(this).parent().parent().find("#ab").attr("class").indexOf("hidden-xs") > 0) {
+                console.log("here");
+                $(this).parent().parent().find("#ego").addClass("hidden-xs");
+                $(this).parent().parent().find("#ab").removeClass("hidden-xs");
+            }
+            else if ($(this).parent().parent().find("#wtb").attr("class").indexOf("hidden-xs") > 0 ) {
+                console.log("here");
+                $(this).parent().parent().find("#pp").addClass("hidden-xs");
+                $(this).parent().parent().find("#wtb").removeClass("hidden-xs");
+            }
+        }
+    });
+
+
 });
 
-function windowSize(){
-    if ($(window).width() <= '974' && $(window).width() >= '751')
-    {
+function windowSize() {
+    if ($(window).width() <= '974' && $(window).width() >= '751') {
         $('#top-col2').detach().insertAfter('#top-col3');
     }
-    else
-    {
+    else {
         $('#top-col3').detach().insertAfter('#top-col2');
     }
 }
